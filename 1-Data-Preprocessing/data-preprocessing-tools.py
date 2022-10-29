ca3cdd03a4769821fa5bd07e646aba248209d98a
Xenomii <voltzxp@gmail.com>
2022-10-29 15:57:57 +0800

    Added multiple linear regression

--- 1-Data-Preprocessing/data-preprocessing-tools.py
+++ 1-Data-Preprocessing/data-preprocessing-tools.py
@@ -13,7 +13,7 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 
 # Determine the independent variables (inputs)
-x = dataset.iloc[:, :-1].values # [All rows, All columns except the last column]
+X = dataset.iloc[:, :-1].values # [All rows, All columns except the last column]
 # Determine the dependent variables (outputs)
 y = dataset.iloc[:, -1].values # [All rows, Only the last column]
 
@@ -32,9 +32,9 @@ from sklearn.impute import SimpleImputer
 # Replace nan values with the average value
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 # Exclude columns with string values for the replacement
-imputer.fit(x[:, 1:3]) # [All rows, Include 2nd and 3rd columns]
+imputer.fit(X[:, 1:3]) # [All rows, Include 2nd and 3rd columns]
 # Perform the replacement and update the dataset values
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # print(x)
 
@@ -53,7 +53,7 @@ from sklearn.preprocessing import OneHotEncoder
 # Apply the encoding technique on the 1st column and keep the other columns
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 # Perform the encoding and convert the return value into an array
-x = np.array(ct.fit_transform(x))
+X = np.array(ct.fit_transform(X))
 
 # print(x)
 
@@ -71,9 +71,9 @@ y = le.fit_transform(y)
 # 5. Splitting dataset into training and test sets
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=1) # random_state is optional (seed)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1) # random_state is optional (seed)
 
-# print(x_train)
+# print(X_train)
 # # [[0.0 0.0 1.0 38.77777777777778 52000.0]
 # #  [0.0 1.0 0.0 40.0 63777.77777777778]
 # #  [1.0 0.0 0.0 44.0 72000.0]
@@ -82,7 +82,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 # #  [1.0 0.0 0.0 48.0 79000.0]
 # #  [0.0 1.0 0.0 50.0 83000.0]
 # #  [1.0 0.0 0.0 35.0 58000.0]]
-# print(x_test)
+# print(X_test)
 # # [[0.0 1.0 0.0 30.0 54000.0]
 # #  [1.0 0.0 0.0 37.0 67000.0]]
 # print(y_train)
@@ -105,11 +105,11 @@ from sklearn.preprocessing import StandardScaler
 
 sc = StandardScaler()
 # Fitting, transforming is performed on the training set (not the encoded column)
-x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
 # Apply the same scaler on the test set
-x_test[:, 3:] = sc.transform(x_test[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
 
-# print(x_train)
+# print(X_train)
 # # [[0.0 0.0 1.0 -0.19159184384578545 -1.0781259408412425]
 # #  [0.0 1.0 0.0 -0.014117293757057777 -0.07013167641635372]
 # #  [1.0 0.0 0.0 0.566708506533324 0.633562432710455]
@@ -118,6 +118,6 @@ x_test[:, 3:] = sc.transform(x_test[:, 3:])
 # #  [1.0 0.0 0.0 1.1475343068237058 1.232653363453549]
 # #  [0.0 1.0 0.0 1.4379472069688968 1.5749910381638885]
 # #  [1.0 0.0 0.0 -0.7401495441200351 -0.5646194287757332]]
-# print(x_test)
+# print(X_test)
 # # [[0.0 1.0 0.0 -1.4661817944830124 -0.9069571034860727]
 # #  [1.0 0.0 0.0 -0.44973664397484414 0.2056403393225306]]
